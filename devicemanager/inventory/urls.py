18a33ac0ac706bf6813e08a29d98d5b941da6e02
Alan Guzek <alan.guzek@swmansion.com>
2024-05-13 23:39:09 +0200

    add model_views for resources

--- devicemanager/inventory/urls.py
+++ devicemanager/inventory/urls.py
@@ -1,7 +1,28 @@
 from django.urls import path
+from rest_framework import routers
 
-from devicemanager.inventory.api_views import qr_code_generate_view
+from devicemanager.inventory.api_views import (
+    BuildingViewSet,
+    DeviceModelViewSet,
+    DeviceTypeViewSet,
+    DeviceViewSet,
+    FacultyViewSet,
+    ManufacturerViewSet,
+    QRCodeGenerateView,
+    RoomViewSet,
+)
 
 app_name = "inventory"
 
-urlpatterns = [path("qr-generate/", qr_code_generate_view, name="qr-generate")]
+qr_code_generate_view = QRCodeGenerateView.as_view()
+
+router = routers.SimpleRouter()
+router.register(r"faculties", FacultyViewSet)
+router.register(r"buildings", BuildingViewSet)
+router.register(r"rooms", RoomViewSet)
+router.register(r"manufacturers", ManufacturerViewSet)
+router.register(r"device-types", DeviceTypeViewSet)
+router.register(r"device-models", DeviceModelViewSet)
+router.register(r"devices", DeviceViewSet)
+
+urlpatterns = [path("qr-generate/", qr_code_generate_view, name="qr-generate"), *router.urls]
